056773cd9477708bf4e7d0cb77c18e840d398771
dave <68038998+dzieger@users.noreply.github.com>
2025-01-16 09:22:59 -0500

    Created test config for parameter beans

---
+++ src/test/java/com/dzieger/config/TestConfig.java
@@ -0,0 +1,49 @@
+package com.dzieger.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+@Configuration
+public class TestConfig {
+
+    @Bean
+    @Qualifier("databaseUrl")
+    public String databaseUrl() {
+        return "jdbc:h2:mem:testdb";
+    }
+
+    @Bean
+    @Qualifier("databaseUsername")
+    public String databaseUsername() {
+        return "sa";
+    }
+
+    @Bean
+    @Qualifier("databasePassword")
+    public String databasePassword() {
+        return "";
+    }
+
+    @Bean
+    @Qualifier("jwtSecret")
+    public String jwtSecret() {
+        return "thisisaverysecuresecretkeyforsigningjwt123";
+    }
+
+    @Bean
+    @Qualifier("jwtExpiration")
+    public String jwtExpiration() {
+        return "3600000";
+    }
+
+    @Bean
+    @Qualifier("jwtIssuer")
+    public String jwtIssuer() {
+        return "testIssuer";
+    }
+
+}
